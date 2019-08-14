82f1a78531c372c0c5979159c3e3c399017e1ca8
Adam Weeks <adweeks@cisco.com>
2019-08-14 11:02:59 -0400

    feat: initial application

---
+++ README.md
@@ -0,0 +1,40 @@
+# Webex Javascript SDK Meetings Quick Start
+
+This sample application demonstrates the basic usage of the Webex Javascript SDK.
+It allows you to create a Webex meeting using the meetings plugin.
+
+## Setup
+
+### Clone the repo to your local machine
+
+```bash
+git clone git@github.com:WebexSamples/webex-meetings-quick-start.git
+```
+
+### Install Dependencies
+
+```bash
+npm install
+```
+
+### Add Personal Access Token
+
+For this example, we'll use your personal access token which can be found in [Getting Started](/docs/api/getting-started/accounts-and-authentication) if you're logged in. This token provides access to your account for testing purposes, and shouldn't be used for anything other than testing. If you don't already have a Webex Teams account, click Sign Up at the top of this page to create a new account.
+
+Open the `index.js` file and add this code to authenticate with Webex Teams, replacing `YOUR_ACCESS_TOKEN` with your development token.
+
+### Start Server
+
+In this example we'll use [Parcel](https://parceljs.org/) to build and serve our web app, including the SDK.
+
+Let's serve the web app with Parcel and start a meeting in the browser!
+
+```bash
+npm start
+```
+
+Open the app ([http://localhost:1234](http://localhost:1234/)) in your browser to use your new web app! 
+Enter the Person ID or email address of who you want to start a meeting with and click the `start meeting` button. 
+Congratulations, you've made your first call in the browser using the Webex Browser SDK!
+
+To see the full example code we have used here to start a meeting, check out the [sample app in GitHub](https://github.com/webex/webex-js-sdk/tree/master/packages/node_modules/samples/browser-single-party-call).
---
+++ index.js
@@ -0,0 +1,110 @@
+const Webex = require('webex');
+
+// Change to your access token from developer.webex.com
+const myAccessToken = 'YOUR_ACCESS_TOKEN';
+
+if (myAccessToken === 'YOUR_ACCESS_TOKEN') {
+  alert('Make sure to update your access token in the index.js file!');
+  throw new Error('Make sure to update your access token in the index.js file!');
+}
+
+const webex = Webex.init({
+  credentials: {
+    access_token: myAccessToken
+  }
+});
+
+webex.config.logger.level = 'debug';
+
+webex.internal.device.register()
+  .then(() => webex.internal.mercury.connect())
+  .catch((err) => {
+    console.error(err);
+    alert(err);
+    throw err;
+  });
+
+function bindMeetingEvents(meeting) {
+  meeting.on('error', (err) => {
+    console.error(err);
+  });
+
+  // Handle media streams changes to ready state
+  meeting.on('media:ready', (media) => {
+    if (!media) {
+      return;
+    }
+    if (media.type === 'local') {
+      document.getElementById('self-view').srcObject = media.stream;
+    }
+    if (media.type === 'remoteVideo') {
+      document.getElementById('remote-view-video').srcObject = media.stream;
+    }
+    if (media.type === 'remoteAudio') {
+      document.getElementById('remote-view-audio').srcObject = media.stream;
+    }
+  });
+
+  // Handle media streams stopping
+  meeting.on('media:stopped', (media) => {
+    // Remove media streams
+    if (media.type === 'local') {
+      document.getElementById('self-view').srcObject = null;
+    }
+    if (media.type === 'remoteVideo') {
+      document.getElementById('remote-view-video').srcObject = null;
+    }
+    if (media.type === 'remoteAudio') {
+      document.getElementById('remote-view-audio').srcObject = null;
+    }
+  });
+
+  // Of course, we'd also like to be able to leave the meeting:
+  document.getElementById('hangup').addEventListener('click', () => {
+    meeting.leave();
+  });
+}
+
+// Join the meeting and add media
+function joinMeeting(meeting) {
+  
+  return meeting.join().then(() => {
+    const mediaSettings = {
+      receiveVideo: true,
+      receiveAudio: true,
+      receiveShare: false,
+      sendVideo: true,
+      sendAudio: true,
+      sendShare: false
+    };
+
+    // Get our local media stream and add it to the meeting
+    return meeting.getMediaStreams(mediaSettings).then((mediaStreams) => {
+      const [localStream, localShare] = mediaStreams;
+
+      meeting.addMedia({
+        localShare,
+        localStream,
+        mediaSettings
+      });
+    });
+  });
+}
+
+document.getElementById('destination').addEventListener('submit', (event) => {
+  // again, we don't want to reload when we try to join
+  event.preventDefault();
+
+  const destination = document.getElementById('invitee').value;
+
+  return webex.meetings.create(destination).then((meeting) => {
+    // Call our helper function for binding events to meetings
+    bindMeetingEvents(meeting);
+
+    return joinMeeting(meeting);
+  })
+  .catch((error) => {
+    // Report the error
+    console.error(error);
+  });
+});
